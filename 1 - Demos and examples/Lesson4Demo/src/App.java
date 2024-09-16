2c0f78de186791f7416cf7296443e2d3679c72f0
cerebus5972 <austin.wang@rcsd.ca>
2024-09-16 08:54:42 -0600

    A3

---
+++ 1 - Demos and examples/Lesson4Demo/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("\tHow is your day going?");
+        System.out.println("It's \"cloudy\" with a chance of meatballs today.");
+    }
+}
