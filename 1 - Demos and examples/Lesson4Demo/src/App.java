defcd43dc436ca7bcdfe66fb297364e5ef5004cd
cerebus5972 <austin.wang@rcsd.ca>
2024-09-16 09:48:17 -0600

    A4

--- 1 - Demos and examples/Lesson4Demo/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("\tHow is your day going?");
-        System.out.println("It's \"cloudy\" with a chance of meatballs today.");
-    }
-}
---
+++ A4 - ASCII Art/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("^___^");
+        System.out.println("|0 0|");
+        System.out.println("| u |");
+        System.out.println("_____");
+        System.out.println("||  ||");
+        System.out.println("m    m");
+        System.out.println(" ____             _____  _______ _____ ");
+        System.out.println("/    \\   |    |  /          |      |   |\\   |");
+        System.out.println("|____|   |    |  \\_____     |      |   | \\  |");
+        System.out.println("|    |   |    |        \\    |      |   |  \\ |");
+        System.out.println("|    |   \\____/   _____/    |    __|__ |   \\|");
+        
+        
+    }
+}
+
