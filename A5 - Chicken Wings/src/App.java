73e97a85da66a10ba1dd1ac76928482e84e7d7ef
cerebus5972 <austin.wang@rcsd.ca>
2024-09-20 09:34:33 -0600

    Chicken Wings

--- A5 - Chicken Wings/src/App.java
+++ A5 - Chicken Wings/src/App.java
@@ -1,5 +1,11 @@
+//Chicken Wings
+//Austin Wang
+//September 20
+//Takes the total money you have as well as a cost of a chicken wing, then returns how many whole chicken wings you can buy with that money. 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double totalmoney = 4.75;
+        double chickencost = 0.75;
+        System.out.println("\n You can buy "+ (int)(totalmoney/chickencost) +" chicken wings with "+ totalmoney +"$. \n");
     }
 }
